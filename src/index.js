a25575b66d553624026287b88f0c9f7dee7ff642
dkzanz1 <paul.zolik@sky.com>
2022-11-13 10:01:42 +0000

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+const myFirstElement = <h1>We can display elements by using html tag equal to const variable</h1>
+  
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(myFirstElement);
+
+
+
